17192e5c8e87e04d92d04509a721606a7a8a274c
Edyr Costa <edyrcosta12@gmail.com>
2020-04-06 23:04:49 -0300

    Tela principal do módulo Settings da solução com a definição da interface e somente o menu com o link para a tela de informações de desenvolvimento funcional.

---
+++ Code/WinThemeChangerSettings/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace WinThemeChangerSettings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow window = new AboutWindow();
+            window.ShowDialog();
+        }
+    }
+}
