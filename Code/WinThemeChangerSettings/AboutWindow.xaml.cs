1ab2910734e13d51bc74122e65fa54981daca39e
Edyr Costa <edyrcosta12@gmail.com>
2020-04-06 21:56:31 -0300

    Tela com informações sobre o programa e o desenvolvimento.

---
+++ Code/WinThemeChangerSettings/AboutWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WinThemeChangerSettings
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+        private void GitHubImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Uri uri = new Uri("https://github.com/SolisInvictus/WinThemeChanger");
+            System.Diagnostics.Process.Start(uri.AbsoluteUri);
+        }
+
+        private void MPLLicense_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Uri uri = new Uri("https://github.com/SolisInvictus/WinThemeChanger/blob/master/LICENSE");
+            System.Diagnostics.Process.Start(uri.AbsoluteUri);
+        }
+    }
+}
