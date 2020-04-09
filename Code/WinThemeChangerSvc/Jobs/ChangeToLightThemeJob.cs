7590d386b6c51a7b59b9b115c15e080e420ac923
Edyr Costa <edyrcosta12@gmail.com>
2020-04-09 12:34:17 -0300

    Criação da classe que faz a alteração para o tema claro no sistema e é executada pelo Scheduler da lib Quartz.NET.

---
+++ Code/WinThemeChangerSvc/Jobs/ChangeToLightThemeJob.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+using WinThemeChangerLib;
+
+namespace WinThemeChangerSvc.Jobs
+{
+    class ChangeToLightThemeJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Change();
+
+            return Task.CompletedTask;
+        }
+
+        private void Change()
+        {
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.CreateSubKey(Consts.PERSONALIZE_REGISTRY_KEY);
+                if (registryKey == null)
+                    throw new Exception();
+
+                Console.WriteLine(registryKey.GetValue(Consts.SYSTEM_SIDE_LIGHT_THEME_KEY));
+                if (Settings.GetInstance().LightChangeWindowMode)
+                    registryKey.SetValue(Consts.SYSTEM_SIDE_LIGHT_THEME_KEY, 1);
+                if (Settings.GetInstance().LightChangeApplicationMode)
+                    registryKey.SetValue(Consts.APPLICATION_SIZE_LIGHT_THEME_KEY, 1);
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Console.WriteLine(e.Message);
+#endif
+            }
+        }
+    }
+}
