8366a09bcb64ff79dbf9b1965e603cfe36d68e92
Edyr Costa <edyrcosta12@gmail.com>
2020-04-09 12:36:44 -0300

    Classe com constantes de uso geral.

---
+++ Code/WinThemeChangerSvc/Consts.cs
@@ -0,0 +1,13 @@
+﻿namespace WinThemeChangerSvc
+{
+    class Consts
+    {
+        public const string THEME_KEY_NAME = "theme";
+        public const string LIGHT_THEME_KEY_VALUE = "light";
+        public const string DARK_THEME_KEY_VALUE = "dark";
+
+        public const string PERSONALIZE_REGISTRY_KEY = @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+        public const string SYSTEM_SIDE_LIGHT_THEME_KEY = "SystemUsesLightTheme";
+        public const string APPLICATION_SIZE_LIGHT_THEME_KEY = "AppsUseLightTheme";
+    }
+}
