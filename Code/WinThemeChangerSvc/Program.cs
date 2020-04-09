7da7441d26b422d36ba1fc27006af550e313694f
Edyr Costa <edyrcosta12@gmail.com>
2020-04-09 12:22:09 -0300

    Adicionado verificação para permitir apenas uma única instância da aplicação por usuário no sistema.

--- Code/WinThemeChangerSvc/Program.cs
+++ Code/WinThemeChangerSvc/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WinThemeChangerSvc
@@ -14,9 +13,23 @@ namespace WinThemeChangerSvc
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainAppContext());
+            var assembly = typeof(Program).Assembly;
+            var attribute = (GuidAttribute)assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0];
+            var id = attribute.Value;
+            using (Mutex mutex = new Mutex(false, id))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("O serviço do Windows 10 Theme Changer já está em execução.", "Serviço já em execução",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                GC.Collect();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainAppContext());
+            }
         }
     }
 }
